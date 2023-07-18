ff2676f521f88fb0697702b41103a1dd56fd6791
rega1237 <rega1237@gmail.com>
2023-07-18 13:14:49 -0400

    config vite

---
+++ vite.config.js
@@ -0,0 +1,14 @@
+import { resolve } from 'path'
+import { defineConfig } from 'vite'
+
+export default defineConfig({
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        presencial: resolve(__dirname, 'presencial.html'),
+        success: resolve(__dirname, 'success.html'),
+      },
+    },
+  },
+})

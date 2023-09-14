1092e6d59ad65a6439ac0cc094da66fef61f9208
rega1237 <rega1237@gmail.com>
2023-09-14 18:06:52 -0400

    emailjs func

--- main.js
+++ main.js
@@ -23,3 +23,47 @@ document.getElementById("checkout").addEventListener("click", (e) => {
   });
 });
 */
+
+function validateEmail(input) {
+  var validRegex =
+    /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+
+  const emailInput = document.getElementById("email").value;
+  const label = document.getElementById("label");
+
+  if (emailInput.match(validRegex)) {
+    return true;
+  } else {
+    label.innerHTML = "Por favor introduce un email valido";
+    setTimeout(() => {
+      label.innerHTML = "";
+    }, 3000);
+    return false;
+  }
+}
+
+window.onload = function () {
+  document
+    .getElementById("contact-form")
+    .addEventListener("submit", function (event) {
+      const emailInput = document.getElementById("email").value;
+      const label = document.getElementById("label");
+      event.preventDefault();
+      if (!validateEmail(emailInput)) return;
+      emailjs
+        .send("service_6yordkl", "template_mfb893u", {
+          email: emailInput,
+        })
+        .then(
+          function (response) {
+            console.log("SUCCESS!", response.status, response.text);
+            document.getElementById("contact-form").reset();
+            label.innerHTML = "Gracias por suscribirte";
+          },
+          function (error) {
+            label.innerHTML = "Ocurrió un error, por favor intenta de nuevo"
+            console.log("FAILED...", error);
+          }
+        );
+    });
+};

94c917a49f235cd155857dcb5bf33fbfda8fb2d2
rega1237 <rega1237@gmail.com>
2024-02-28 13:26:35 -0400

    make a function to reuse in main

--- javascript/online.js
+++ javascript/online.js
@@ -1,27 +1,35 @@
-// Modal Taller
+const modalOnlinePrices = () => {
+  // Modal Taller
 
-const buttonShow = document.getElementById("button-show-taller");
-const modalTaller = document.getElementById("modal-taller");
-const closeButton = document.getElementById("close-button-taller")
+  const buttonShow = document.getElementById("button-show-taller");
+  const modalTaller = document.getElementById("modal-taller");
+  const closeButton = document.getElementById("close-button-taller");
 
-// Modal Taller + session
+  // Modal Taller + session
 
-const buttonShowTallerSesion = document.getElementById("button-show-taller-sesion");
-const modalTallerSesion = document.getElementById("modal-taller-sesion");
-const closeButtonTallerSesion = document.getElementById("close-button-taller-sesion")
+  const buttonShowTallerSesion = document.getElementById(
+    "button-show-taller-sesion"
+  );
+  const modalTallerSesion = document.getElementById("modal-taller-sesion");
+  const closeButtonTallerSesion = document.getElementById(
+    "close-button-taller-sesion"
+  );
 
-buttonShow.addEventListener("click", () => {
-  modalTaller.classList.remove("hidden")
-})
+  buttonShow.addEventListener("click", () => {
+    modalTaller.classList.remove("hidden");
+  });
 
-closeButton.addEventListener("click", () => {
-  modalTaller.classList.toggle("hidden")
-});
+  closeButton.addEventListener("click", () => {
+    modalTaller.classList.toggle("hidden");
+  });
 
-buttonShowTallerSesion.addEventListener("click", () => {
-  modalTallerSesion.classList.remove("hidden")
-})
+  buttonShowTallerSesion.addEventListener("click", () => {
+    modalTallerSesion.classList.remove("hidden");
+  });
 
-closeButtonTallerSesion.addEventListener("click", () => {
-  modalTallerSesion.classList.toggle("hidden")
-});
+  closeButtonTallerSesion.addEventListener("click", () => {
+    modalTallerSesion.classList.toggle("hidden");
+  });
+};
+
+export default modalOnlinePrices
